9e8cd7e8444b0791cd2e60d520a692ee7704ed21
tian <596893301@qq.com>
2019-09-12 11:03:47 +0800

    新增一些通用方法

---
+++ utils/src/main/java/com/example/utils/ICoordinates.java
@@ -0,0 +1,8 @@
+package com.example.utils;
+
+public interface ICoordinates {
+    public double getLat();
+    public double getLon();
+    public void setLat(double lat);
+    public void setLon(double lon);
+}
