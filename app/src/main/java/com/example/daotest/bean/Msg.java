1238e5d6349d561363819353b15fd30f698f4716
tian <596893301@qq.com>
2019-09-12 08:34:02 +0800

    修改名称

--- app/src/main/java/com/example/daotest/bean/Msg.java
+++
@@ -1,9 +0,0 @@
-package com.example.daotest.bean;
-
-/**
- * @author tian on 2019/9/3
- */
-public class Msg {
-    public String text;
-    public String from;
-}
--- app/src/main/java/com/example/daotest/util/ICoordinates.java
+++
@@ -1,8 +0,0 @@
-package com.example.daotest.util;
-
-public interface ICoordinates {
-    public double getLat();
-    public double getLon();
-    public void setLat(double lat);
-    public void setLon(double lon);
-}
